78f9ca434a4c432d7c64148743470c1ccc253df4
Haseebullah-Shaikh-Official <haseebullah.shaikh@xloopdigital.com>
2023-06-21 19:41:11 +0500

    updated expose api service

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-### to check final results in db 
-docker exec -it postgres-container psql -U user outcome_prediction
+# Group-E-Outcome-Prediction - Capstone
+
+#### Group E - Team Members
+###### Haseebullah Shaikh (Lead)
+###### Humza Asim
+###### Hammad Irshad
+###### Humza Moeen
+###### Huzaifa Waseem
+
+### Project details will be updated later
--- src/expose-api/api.py
+++ src/expose-api/api.py
@@ -1,11 +1,14 @@
 import psycopg2
+import uvicorn
 from fastapi import FastAPI, HTTPException
+from psycopg2 import errors
 
 api = FastAPI()
+query = 'SELECT * FROM result WHERE "councillor_id" = %s'
 
 
-@api.get("/data/{id}")
-async def get_data(id: int):
+@api.get("/{id}")
+def get_data(id: int):
     try:
         # Connect to the PostgreSQL database
         conn = psycopg2.connect(
@@ -13,15 +16,14 @@ async def get_data(id: int):
             port=5432,
             database="outcome_prediction",
             user="user",
-            password="password"
+            password="password",
         )
 
         # Creating a cursor
         cur = conn.cursor()
 
         # Execute the SQL query with the specified ID
-        cur.execute("SELECT * FROM result WHERE \"councillor_id\" = %s", (id,))
-        
+        cur.execute(query, (id,))
 
         # Fetch the row
         row = cur.fetchone()
@@ -41,16 +43,27 @@ async def get_data(id: int):
         data = {column: value for column, value in zip(columns, row)}
 
         # Return the fetched data
-        return {"data": data}
+        return data
 
-    except (Exception, psycopg2.Error) as error:
-        if str(error) == "":
-            return {"error": "id is not available"}
+    except psycopg2.OperationalError as error:
+        if isinstance(error, errors.UndefinedTable):
+            # Handle the case when the table does not exist
+            raise HTTPException(status_code=404, detail="Table not found")
+        elif isinstance(error, errors.DatabaseError):
+            # Handle the case when the database does not exist
+            raise HTTPException(status_code=404, detail="Database not found")
         else:
-            return {"error": str(error)}
+            # Handle other operational errors
+            raise HTTPException(status_code=500, detail=str(error))
+
+    except psycopg2.ProgrammingError as error:
+        # Handle programming errors (e.g., syntax error, invalid query)
+        raise HTTPException(status_code=400, detail=str(error))
+
+    except Exception as error:
+        # Handle other specific exceptions
+        raise HTTPException(status_code=500, detail="An error occurred") from error
 
 
 if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(api, host="0.0.0.0", port=8000)
+    uvicorn.run("api:api", host="0.0.0.0", port=8000)
